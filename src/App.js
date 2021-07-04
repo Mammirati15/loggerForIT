2ea08618a55006b740d0b536fdccf03ae1ca3ba8
Matt Ammirati <matt.ammirati@gmail.com>
2021-07-03 14:30:41 -1000

    set up materialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import {useEffect} from 'react'
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js'
 import './App.css';
 
-function App() {
+const App = () => {
+  //how you initialize JS from Materialize
+  useEffect(() => {
+    M.autoInit()
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Into the Abysssssssss
     </div>
   );
 }
