61cd1e754711c461a2d95c26c5b450a91caf842e
Matt Ammirati <matt.ammirati@gmail.com>
2021-07-05 14:35:09 -1000

    added a modal to enter message and tech, also a toast for all fields filled

---
+++ src/components/logs/AddLogModal.js
@@ -0,0 +1,66 @@
+import React, {useState} from 'react'
+import M from 'materialize-css/dist/js/materialize.min.js'
+
+const AddLogModal = () => {
+    const [message, setMessage] = useState('')
+    const [attention, setAttention] = useState(false)
+    const [tech, setTech] = useState('')
+
+    const onSubmit = () => {
+        if (message === '' || tech === ''){
+            M.toast({html: 'Please enter a message and a tech', classes: 'rounded'})
+        } else {
+            console.log(message, tech, attention)
+        }       
+    }
+
+    return (
+        <div id='add-log-modal' className="modal" style={modalStyle}>
+            <div className="modal-content">
+                <h4>Enter System Log</h4>
+                <div className="row">
+                    <div className="input-field">
+                        <input type="text" name="message" value={message} onChange={e => setMessage(e.target.value)} />
+                        <label htmlFor="message" className="active">
+                            Log Message
+                        </label>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="input-field">
+                        <select name="tech" value={tech} className="browser-default" onChange={e => setTech(e.target.value)}>
+                            <option value='' disabled>
+                                Select Technicion
+                            </option>
+                            <option value="John Jovie">John Jovie</option>
+                            <option value="Steve Buschemi">Steve Buschemi</option>
+                            <option value="Tom Hanks">Tom Hanks</option>
+                            <option value="Herman Muenster">Herman Muenster</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="input-field">
+                        <p>
+                            <label>
+                                <input type="checkbox" className="filled-in" checked={attention} value={attention} onChange={e => setAttention(!attention)} />
+                                <span>Needs Attention</span>
+                            </label>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="modal-footer">
+                <a href="#!" onClick={onSubmit} className="waves-effect waves-light btn green"><i className="material-icons left">cloud</i>Enter</a> 
+            </div>
+        </div>
+    )
+}
+
+const modalStyle = {
+    width: '75%',
+    height: '75%'
+}
+
+export default AddLogModal
+
